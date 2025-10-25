2cac7bcd3fb5c34f698d290745acb1194dce5d9e
Lucas Sain <sainlucas250@gmail.com>
2025-10-25 17:56:35 -0300

    Estructura login
    Definimos la estructura para el login en usuarios.h

--- CodePlay_Store_proyectoFinal_PrograI/usuarios.h
+++ CodePlay_Store_proyectoFinal_PrograI/usuarios.h
@@ -1,6 +1,13 @@
 #ifndef USUARIOS_H_INCLUDED
 #define USUARIOS_H_INCLUDED
+#define DIMENSION_LOGIN 30
 
+typedef struct
+{
+    char usuario[DIMENSION_LOGIN];
+    char email [DIMENSION_LOGIN];
+    char contrasenia [DIMENSION_LOGIN];
+}stLogin;
 
 
 #endif // USUARIOS_H_INCLUDED
