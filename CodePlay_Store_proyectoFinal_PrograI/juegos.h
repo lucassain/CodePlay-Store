923b50c3780aa1664cfcda3d0dd5659f4dc24a79
Lucas Sain <sainlucas250@gmail.com>
2025-11-08 23:32:21 -0300

    juegos cargados

--- CodePlay_Store_proyectoFinal_PrograI/juegos.h
+++ CodePlay_Store_proyectoFinal_PrograI/juegos.h
@@ -2,14 +2,20 @@
 #define JUEGOS_H_INCLUDED
 
 #define ARCHIVO_JUEGOS "catalogo.dat"
+#define ARCHIVO_PLATAFORMAS "plataformas.dat"
 
 typedef struct {
     int id;
     char nombre[30];
     char genero[20];
     float precio;
-    int idEmpresaDuena;
+    int idEmpresa;
 } stJuego;
+
+typedef struct {
+    int idJuego;
+    char nombre[20];
+} stPlataforma;
 
 // PROTOTIPADO
 void catalogoJuegos();
